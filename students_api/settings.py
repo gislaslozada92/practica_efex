91d5e9fbf65edde8090ca156dc0ad17ef1791b6f
Guillermo Islas <guillermo.islas@netcity.mx>
2025-04-08 12:47:08 -0600

    algo

--- students_api/settings.py
+++ students_api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "students.adapters.persistence.apps.StudentsConfig",
+    "students.adapters.persistence.apps.StudentsPersistenceConfig",
 ]
 
 MIDDLEWARE = [
